fc37acbafc6bc83cc56f342b3d6f514bd336c05c
Leo Tellier <leotellier62360@gmail.com>
2024-11-22 09:25:52 +0100

    modif code map (plus simple)

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const map = L.map('map').setView([51.505, -0.09], 13);
+const map = L.map('map').setView([50.72825092034062, 1.6105813421016573], 8);
     const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
